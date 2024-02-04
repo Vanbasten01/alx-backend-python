afe36af59648d2b29b2ba7c5b713d6782eb0fa2c
Vanbasten01 <fouad.yasin01@gmail.com>
2024-02-04 18:11:10 +0000

    Parameterize and patch as decorators

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -3,7 +3,7 @@
 
 import unittest
 from parameterized import parameterized
-from unittest.mock import patch, MagicMock
+from unittest.mock import patch
 from client import GithubOrgClient
 
 
@@ -18,7 +18,6 @@ class TestGithubOrgClient(unittest.TestCase):
         """Test the org method of GithubOrgClient class."""
         test_inst = GithubOrgClient(org_name)
         test_inst.org()
-        # Assert that the get_json method was called with the expected URL
         mock_get_json.assert_called_once_with(
                 f"https://api.github.com/orgs/{org_name}")
 
